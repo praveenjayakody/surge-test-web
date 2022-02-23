5200f62e5d7a3e1a954198de3dc3b35dbd689b4a
OrpheusOnApollo <3007446+praveenjayakody@users.noreply.github.com>
2022-02-24 00:30:20 +0530

    Bug fix

--- src/util/XStorage.js
+++ src/util/XStorage.js
@@ -20,20 +20,21 @@ export class XStorage {
         if (val === null && returnNull) return val;
         if (val === "true") return true;
         if (typeof val === "undefined" || val === null || val === "false") return false;
+        return val;
     }
-    static load(key, prefix = 'grocer') {
+    static load(key, prefix = 'todo') {
         /**
          * Difference between load and get is that load returns null if the key was not found
          */
         return this._parseValue(localStorage.getItem((prefix !== "" ? prefix+".":"") + key), true);
     }
-    static get(key, prefix = 'grocer') {
+    static get(key, prefix = 'todo') {
         /**
          * If a prefix is provided, system assumes the key is stored with the prefix preceding the key
          */
         return this._parseValue(localStorage.getItem((prefix !== "" ? prefix+".":"") + key));
     }
-    static set(key, value, prefix = 'grocer') {
+    static set(key, value, prefix = 'todo') {
         localStorage.setItem((prefix !== "" ? prefix+".":"") + key, value);
     }
 
